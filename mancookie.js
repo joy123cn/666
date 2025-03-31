fcc17efbc97d7a4eab792af40cd66c2b1ccdeec4
joy123cn <zhousevina@gmail.com>
2025-03-31 18:57:20 +0800

    Delete mancookie.js

--- mancookie.js
+++
@@ -1,9 +0,0 @@
-// Quantumult X 自动抓取最新 Cookie 并保存
-var cookie = $request.headers["Cookie"];
-if (cookie) {
-    $persistentStore.write(cookie, "mancookie");  // 把 Cookie 存储到本地
-    $notify("Manwa Cookie 更新成功 ✅", "", "新 Cookie 已保存！");
-} else {
-    $notify("Manwa Cookie 更新失败 ❌", "", "请手动检查网站是否变更！");
-}
-$done();
