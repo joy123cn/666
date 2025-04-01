720b2b9b0fa072af527dd0fba703ea0a0be01e34
joy123cn <zhousevina@gmail.com>
2025-04-01 23:25:03 +0800

    更新 Emby.js

--- grammar/Emby.js
+++ grammar/Emby.js
@@ -6,10 +6,10 @@
 使用方法：手动观看一次，提示获取成功✅即可食用|可多账号获取
 获取完参数请手动关闭重写
 [rewrite_local]
-^https?:\/\/.+\/emby\/Sessions\/Playing\/Stopped url script-request-body https://raw.githubusercontent.com/joy123cn/666/main/grammar/emby.js
+^https?:\/\/.+\/emby\/Sessions\/Playing\/Stopped url script-request-body https://raw.githubusercontent.com/joy123cn/666/main/grammar/Emby.js
 
 [task_local]
-35 22 15,30 * * https://raw.githubusercontent.com/joy123cn/666/main/grammar/emby.js, tag=Emby自动观看, img-url=https://raw.githubusercontent.com/fmz200/wool_scripts/main/icons/lige47/emby.png, enabled=true
+35 22 15,30 * * https://raw.githubusercontent.com/joy123cn/666/main/grammar/Emby.js, tag=Emby自动观看, img-url=https://raw.githubusercontent.com/fmz200/wool_scripts/main/icons/lige47/emby.png, enabled=true
 
 [MITM]
 hostname = -*.fileball.app, *
