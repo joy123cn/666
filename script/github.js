0413c385afcc42cbb476cf2154389df21c916dfb
joy123cn <zhousevina@gmail.com>
2025-05-05 16:17:48 +0800

    创建 github.js

---
+++ script/github.js
@@ -0,0 +1,6 @@
+[rewrite_local]
+^https:\/\/(raw|gist)\.githubusercontent\.com\/ url request-header (\r\n)Accept-Language:.+(\r\n) request-header $1Accept-Language: en-us$2
+^https:\/\/github\.com\/ url request-header (\r\n)Accept-Language:.+(\r\n) request-header $1Accept-Language: en-us$2
+
+[mitm]
+hostname = raw.githubusercontent.com,gist.githubusercontent.com,github.com
